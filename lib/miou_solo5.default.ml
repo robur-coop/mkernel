30eb732568cf9ae7e75ac2cdb23fb26f42e7eab8
Calascibetta Romain <romain.calascibetta@gmail.com>
2025-03-17 13:51:28 +0100

    Continue

--- lib/miou_solo5.default.ml
+++ lib/miou_solo5.default.ml
@@ -1,9 +1,58 @@
+let src = Logs.Src.create "miou.solo5"
+
+module Log = (val Logs.src_log src : Logs.LOG)
+
+exception Solo5_error of int * string * string
+
+let _ =
+  Callback.register_exception "Solo5.Solo5_error" (Solo5_error (0, "", ""))
+
 type bigstring =
   (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+
+external bigstring_get_uint8 : bigstring -> int -> int = "%caml_ba_ref_1"
+
+external bigstring_set_uint8 : bigstring -> int -> int -> unit
+  = "%caml_ba_set_1"
+
+external bigstring_get_int32_ne : bigstring -> int -> int32
+  = "%caml_bigstring_get32"
+
+external bigstring_set_int32_ne : bigstring -> int -> int32 -> unit
+  = "%caml_bigstring_set32"
+
+let bigstring_blit_to_bytes bstr ~src_off dst ~dst_off ~len =
+  let len0 = len land 3 in
+  let len1 = len lsr 2 in
+  for i = 0 to len1 - 1 do
+    let i = i * 4 in
+    let v = bigstring_get_int32_ne bstr (src_off + i) in
+    Bytes.set_int32_ne dst (dst_off + i) v
+  done;
+  for i = 0 to len0 - 1 do
+    let i = (len1 * 4) + i in
+    let v = bigstring_get_uint8 bstr (src_off + i) in
+    Bytes.set_uint8 dst (dst_off + i) v
+  done
+
+let bigstring_blit_from_string src ~src_off dst ~dst_off ~len =
+  let len0 = len land 3 in
+  let len1 = len lsr 2 in
+  for i = 0 to len1 - 1 do
+    let i = i * 4 in
+    let v = String.get_int32_ne src (src_off + i) in
+    bigstring_set_int32_ne dst (dst_off + i) v
+  done;
+  for i = 0 to len0 - 1 do
+    let i = (len1 * 4) + i in
+    let v = String.get_uint8 src (src_off + i) in
+    bigstring_set_uint8 dst (dst_off + i) v
+  done
 
 [@@@warning "-32-34-37"]
 
 let error_msgf fmt = Fmt.kstr (fun msg -> Error (`Msg msg)) fmt
+let failwithf fmt = Fmt.kstr failwith fmt
 let failwith_error = function Ok () -> () | Error msg -> Fmt.failwith "%s" msg
 
 module Json = struct
@@ -129,6 +178,18 @@ let load_devices filename =
   let* profiling = Jsont_bytesrw.decode_string json str in
   Ok (setup_profiling := Some profiling)
 
+let find_net_device name =
+  match !setup_profiling with
+  | None -> failwithf "Not devices specified"
+  | Some { devices; _ } -> (
+      let fn = function
+        | { name= name'; value= Net _ } -> String.equal name name'
+        | _ -> false
+      in
+      match List.find_opt fn devices with
+      | Some { value= Net { interface; _ }; _ } -> interface
+      | _ -> failwithf "Net device %s not found" name)
+
 let () =
   match Sys.getenv_opt "MIOU_PROFILING" with
   | Some filename when existing_file filename ->
@@ -140,16 +201,187 @@ let to_json = function
       `O [ ("name", `String name); ("type", `String "BLOCK_BASIC") ]
   | `Net name -> `O [ ("name", `String name); ("type", `String "NET_BASIC") ]
 
+external miou_solo5_net_connect : string -> (int[@untagged])
+  = "unimplemented" "miou_solo5_net_connect"
+[@@noalloc]
+
+external unsafe_get_int64_ne : bytes -> int -> int64 = "%caml_bytes_get64u"
+
+external miou_solo5_net_read :
+     (int[@untagged])
+  -> bigstring
+  -> (int[@untagged])
+  -> (int[@untagged])
+  -> bytes
+  -> int = "unimplemented" "miou_solo5_net_read"
+[@@noalloc]
+
+external miou_solo5_net_write :
+  (int[@untagged]) -> (int[@untagged]) -> (int[@untagged]) -> bigstring -> unit
+  = "unimplemented" "miou_solo5_net_write"
+[@@noalloc]
+
+external miou_solo5_select : int list -> float -> int list
+  = "unimplemented" "miou_solo5_select"
+
+module File_descrs = struct
+  type 'a t = { mutable contents: (int * 'a) list }
+
+  let find tbl fd = List.assq fd tbl.contents
+
+  let replace tbl fd v' =
+    let contents =
+      List.fold_left
+        (fun acc (k, v) -> if k = fd then (k, v') :: acc else (k, v) :: acc)
+        [] tbl.contents
+    in
+    tbl.contents <- contents
+
+  let add tbl k v = tbl.contents <- (k, v) :: tbl.contents
+  let create _ = { contents= [] }
+
+  let append t k v =
+    try
+      let vs = find t k in
+      replace t k (v :: vs)
+    with Not_found -> add t k [ v ]
+
+  let fold_left_map fn acc t =
+    let acc, contents = List.fold_left_map fn acc t.contents in
+    t.contents <- contents;
+    acc
+
+  let filter_map fn t =
+    let contents = List.filter_map fn t.contents in
+    t.contents <- contents
+
+  let iter fn t = List.iter (fun (k, v) -> fn k v) t.contents
+end
+
+type elt = { time: int; syscall: Miou.syscall; mutable cancelled: bool }
+
+module Heapq = Miou.Pqueue.Make (struct
+  type t = elt
+
+  let dummy = { time= 0; syscall= Obj.magic (); cancelled= false }
+  let compare { time= a; _ } { time= b; _ } = Int.compare a b
+end)
+
+type domain = { fds: Miou.syscall list File_descrs.t; sleepers: Heapq.t }
+
+let domain = { fds= File_descrs.create 0x100; sleepers= Heapq.create () }
+
+let blocking_read fd =
+  let syscall = Miou.syscall () in
+  let fn () = File_descrs.append domain.fds fd syscall in
+  Miou.suspend ~fn syscall
+
 module Net = struct
   type t = int
   type mac = string
   type cfg = { mac: mac; mtu: int }
 
-  let connect _name = assert false
-  let read_bigstring _t ?off:_ ?len:_ _bstr = assert false
-  let read_bytes _t ?off:_ ?len:_ _buf = assert false
-  let write_bigstring _t ?off:_ ?len:_ _bstr = assert false
-  let write_string _t ?off:_ ?len:_ _str = assert false
+  let connect name =
+    Log.debug (fun m -> m "search %s device" name);
+    let name = find_net_device name in
+    Log.debug (fun m -> m "attach tap interface %s" name);
+    match miou_solo5_net_connect name with
+    | -1 -> error_msgf "Interface %s not found" name
+    | -2 -> error_msgf "Interface %s is not up" name
+    | -3 -> error_msgf "Impossible to open %s" name
+    | -4 -> error_msgf "Impossible to attach %s" name
+    | -5 -> error_msgf "Invalid attached TAP interface"
+    | fd ->
+        let v = Random.int64 Int64.max_int in
+        let buf = Bytes.create 8 in
+        Bytes.set_int64_be buf 0 v;
+        Bytes.set_uint8 buf 0 (Bytes.get_uint8 buf 0 land 0xfe);
+        Bytes.set_uint8 buf 0 (Bytes.get_uint8 buf 0 lor 0x02);
+        let mac = Bytes.sub_string buf 0 6 in
+        let cfg = { mac; mtu= 1500 } in
+        Ok (fd, cfg)
+
+  let read t ~off ~len bstr =
+    let rec go read_size =
+      blocking_read t;
+      let result = miou_solo5_net_read t bstr off len read_size in
+      match result with
+      | 0 -> Int64.to_int (unsafe_get_int64_ne read_size 0)
+      | 1 -> blocking_read t; go read_size
+      | 2 -> invalid_arg "Miou_solo5.Net.read"
+      | _ -> assert false (* UNSPEC *)
+    in
+    go (Bytes.make 8 '\000')
+
+  let read_bigstring t ?(off = 0) ?len bstr =
+    let len =
+      match len with Some len -> len | None -> Bigarray.Array1.dim bstr - off
+    in
+    if len < 0 || off < 0 || off > Bigarray.Array1.dim bstr - len then
+      invalid_arg "Miou_solo5.Net.read_bigstring: out of bounds";
+    read t ~off ~len bstr
+
+  let read_bytes =
+    (* NOTE(dinosaure): Using [bstr] as a global is safe for 2 reasons. We
+       don't have several domains with Solo5, so there can't be a data-race on
+       this value. Secondly, we ensure that as soon as Solo5 writes to it, we
+       save the bytes in the buffer given by the user without giving the
+       scheduler a chance to execute another task (such as another
+       [read_bytes]). *)
+    let bstr = Bigarray.(Array1.create char c_layout 0x7ff) in
+    fun t ?(off = 0) ?len buf ->
+      let read_size = Bytes.make 8 '\000' in
+      let rec go dst_off dst_len =
+        if dst_len > 0 then begin
+          let len = Int.min (Bigarray.Array1.dim bstr) dst_len in
+          let result = miou_solo5_net_read t bstr off len read_size in
+          match result with
+          | 0 ->
+              let len = Int64.to_int (unsafe_get_int64_ne read_size 0) in
+              bigstring_blit_to_bytes bstr ~src_off:0 buf ~dst_off ~len;
+              if len > 0 then go (dst_off + len) (dst_len - len)
+              else dst_off - off
+          | 1 -> blocking_read t; go dst_off dst_len
+          | 2 -> invalid_arg "Miou_solo5.Net.read"
+          | _ -> assert false (* UNSPEC *)
+        end
+        else dst_off - off
+      in
+      let len =
+        match len with Some len -> len | None -> Bytes.length buf - off
+      in
+      if len < 0 || off < 0 || off > Bytes.length buf - len then
+        invalid_arg "Miou_solo5.Net.read_bytes: out of bounds";
+      go off len
+
+  let write t ~off ~len bstr = miou_solo5_net_write t off len bstr
+
+  let write_bigstring t ?(off = 0) ?len bstr =
+    let len =
+      match len with Some len -> len | None -> Bigarray.Array1.dim bstr - off
+    in
+    if len < 0 || off < 0 || off > Bigarray.Array1.dim bstr - len then
+      invalid_arg "Miou_solo5.Net.write_bigstring: out of bounds";
+    write t ~off ~len bstr
+
+  let write_string =
+    let bstr = Bigarray.(Array1.create char c_layout 0x7ff) in
+    fun t ?(off = 0) ?len str ->
+      let rec go src_off src_len =
+        if src_len > 0 then begin
+          let len = Int.min (Bigarray.Array1.dim bstr) src_len in
+          bigstring_blit_from_string str ~src_off bstr ~dst_off:0 ~len;
+          write_bigstring t ~off:0 ~len bstr;
+          Miou.yield ();
+          go (src_off + len) (src_len - len)
+        end
+      in
+      let len =
+        match len with Some len -> len | None -> String.length str - off
+      in
+      if len < 0 || off < 0 || off > String.length str - len then
+        invalid_arg "Miou_solo5.Net.write_string: out of bounds";
+      go off len
 end
 
 module Block = struct
@@ -171,12 +403,88 @@ external clock_wall : unit -> (int[@untagged])
   = "unimplemented" "miou_solo5_clock_wall"
 [@@noalloc]
 
-let sleep _ = assert false
+let sleep until =
+  let syscall = Miou.syscall () in
+  let elt = { time= clock_monotonic () + until; syscall; cancelled= false } in
+  Heapq.insert elt domain.sleepers;
+  Miou.suspend syscall
+
+let rec sleeper domain =
+  match Heapq.find_min_exn domain.sleepers with
+  | exception Heapq.Empty -> None
+  | { cancelled= true; _ } ->
+      Heapq.delete_min_exn domain.sleepers;
+      sleeper domain
+  | { time; _ } -> Some time
+
+let in_the_past t = t == 0 || t <= clock_monotonic ()
+
+let rec collect_sleepers domain signals =
+  match Heapq.find_min_exn domain.sleepers with
+  | exception Heapq.Empty -> signals
+  | { cancelled= true; _ } ->
+      Heapq.delete_min_exn domain.sleepers;
+      collect_sleepers domain signals
+  | { time; syscall; _ } when in_the_past time ->
+      Heapq.delete_min_exn domain.sleepers;
+      collect_sleepers domain (Miou.signal syscall :: signals)
+  | _ -> signals
+
+let collect_file_descrs file_descrs domain signals =
+  let fn acc (file_descr, syscalls) =
+    if List.exists (Int.equal file_descr) file_descrs then
+      let signals = List.rev_map Miou.signal syscalls in
+      (List.rev_append signals acc, (file_descr, []))
+    else (acc, (file_descr, syscalls))
+  in
+  File_descrs.fold_left_map fn signals domain.fds
+
+let clean domain uids =
+  let to_delete syscall =
+    let uid = Miou.uid syscall in
+    List.exists (fun uid' -> uid == uid') uids
+  in
+  let fn0 (handle, syscalls) =
+    match List.filter (Fun.negate to_delete) syscalls with
+    | [] -> None
+    | syscalls -> Some (handle, syscalls)
+  in
+  let fn1 (({ syscall; _ } : elt) as elt) =
+    if to_delete syscall then elt.cancelled <- true
+  in
+  File_descrs.filter_map fn0 domain.fds;
+  Heapq.iter fn1 domain.sleepers
+
+let file_descrs tbl =
+  let res = ref [] in
+  File_descrs.iter (fun k _ -> res := k :: !res) tbl;
+  !res
+
+let select ~block cancelled_syscalls =
+  clean domain cancelled_syscalls;
+  let timeout =
+    match (sleeper domain, block) with
+    | None, true -> -1.0
+    | (None | Some _), false -> 0.0
+    | Some value, true ->
+        let value = value - clock_monotonic () in
+        let value = Int.max value 0 in
+        Float.of_int value /. 1e-9
+  in
+  let rds = file_descrs domain.fds in
+  match miou_solo5_select rds timeout with
+  | [] -> collect_sleepers domain []
+  | rds ->
+      let signals = collect_sleepers domain [] in
+      collect_file_descrs rds domain signals
+
+let events _domain = { Miou.interrupt= ignore; select; finaliser= ignore }
 
 type 'a arg =
-  | Args : ('k, 'res) devices -> 'a arg
+  | Args : ('k, 'res) devices * 'k -> 'res arg
   | Block : string -> Block.t arg
   | Net : string -> (Net.t * Net.cfg) arg
+  | Const : 'a -> 'a arg
 
 and ('k, 'res) devices =
   | [] : (unit -> 'res, 'res) devices
@@ -184,8 +492,8 @@ and ('k, 'res) devices =
 
 let net name = Net name
 let block name = Block name
-let map _fn args = Args args
-let const _ = Args []
+let map fn args = Args (args, fn)
+let const v = Const v
 
 type t = [ `Block of string | `Net of string ]
 
@@ -195,19 +503,49 @@ let collect devices =
      | [] -> List.rev acc
      | Block name :: rest -> go (`Block name :: acc) rest
      | Net name :: rest -> go (`Net name :: acc) rest
-     | Args vs :: rest -> go (go acc vs) rest
+     | Args (vs, _) :: rest -> go (go acc vs) rest
+     | Const _ :: rest -> go acc rest
   in
   go [] devices
 
-let run ?g:_ args _fn =
-  let devices = collect args in
-  let v =
-    `O
-      List.
-        [
-          ("type", `String "solo5.manifest"); ("version", `Float 1.0)
-        ; ("devices", `A (List.map to_json devices))
-        ]
-  in
-  let output str = output_string stdout str in
-  Json.encode ~output v; exit 0
+let rec ctor : type a. a arg -> a = function
+  | Args (args, fn) -> go (fun fn -> fn ()) args fn
+  | Net device -> begin
+      match Net.connect device with
+      | Ok (t, cfg) -> (t, cfg)
+      | Error (`Msg msg) -> failwithf "%s." msg
+    end
+  | Block device -> begin
+      match Block.connect device with
+      | Ok t -> t
+      | Error (`Msg msg) -> failwithf "%s." msg
+    end
+  | Const v -> v
+
+and go : type k res. ((unit -> res) -> res) -> (k, res) devices -> k -> res =
+ fun run -> function
+  | [] -> fun fn -> run fn
+  | arg :: devices ->
+      let v = ctor arg in
+      fun fn ->
+        let r = fn v in
+        go run devices r
+
+let run ?g args fn =
+  match !setup_profiling with
+  | Some _profiling ->
+      Miou.run ~events ~domains:0 ?g @@ fun () ->
+      let run fn = fn () in
+      go run args fn
+  | None ->
+      let devices = collect args in
+      let v =
+        `O
+          List.
+            [
+              ("type", `String "solo5.manifest"); ("version", `Float 1.0)
+            ; ("devices", `A (List.map to_json devices))
+            ]
+      in
+      let output str = output_string stdout str in
+      Json.encode ~output v; exit 0
--- lib/stubs.default.c
+++ lib/stubs.default.c
@@ -1,21 +1,245 @@
+#include <caml/bigarray.h>
 #include <caml/memory.h>
+#include <sys/ioctl.h>
+#include <ifaddrs.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
 
 #ifndef __unused
-# if defined(_MSC_VER) && _MSC_VER >= 1500
-#  define __unused(x) __pragma( warning (push) ) \
-    __pragma( warning (disable:4189 ) ) \
-    x \
-    __pragma( warning (pop))
-# else
-#  define __unused(x) x __attribute__((unused))
-# endif
+#if defined(_MSC_VER) && _MSC_VER >= 1500
+#define __unused(x)                                                            \
+  __pragma(warning(push)) __pragma(warning(disable : 4189)) x __pragma(        \
+      warning(pop))
+#else
+#define __unused(x) x __attribute__((unused))
+#endif
 #endif
 #define __unit() value __unused(unit)
 
-intnat miou_solo5_clock_monotonic(__unit ()) {
+#if defined(__linux__)
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <sys/socket.h>
+#include <time.h>
+#elif defined(__FreeBSD__)
+#include <net/if.h>
+#elif defined(__OpenBSD__)
+#include <net/if.h>
+#include <sys/socket.h>
+#else
+#error Unsupported system
+#endif
+
+intnat miou_solo5_net_connect(value vifname) {
+  const char *ifname = String_val(vifname);
+  struct ifaddrs *ifa, *ifp;
+  int fd;
+  int found;
+  int up;
+
+  if (getifaddrs(&ifa) == -1)
+    return -1;
+  ifp = ifa;
+  while (ifp) {
+    if (strncmp(ifp->ifa_name, ifname, IFNAMSIZ) == 0) {
+      found = 1;
+      up = ifp->ifa_flags & (IFF_UP | IFF_RUNNING);
+      break;
+    }
+    ifp = ifp->ifa_next;
+  }
+  freeifaddrs(ifa);
+  if (!found)
+    return (-1);
+
+#if defined(__linux__)
+
+  if (!up)
+    return (-2);
+
+  struct ifreq ifr;
+  fd = open("/dev/net/tun", O_RDWR | O_NONBLOCK);
+  if (fd == -1)
+    return (-3);
+
+  memset(&ifr, 0, sizeof(ifr));
+  ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
+  strncpy(ifr.ifr_name, ifname, IFNAMSIZ - 1);
+
+  if (ioctl(fd, TUNSETIFF, (void *)&ifr) == -1) {
+    close(fd);
+    return (-4);
+  }
+
+  if (strncmp(ifr.ifr_name, ifname, IFNAMSIZ) != 0) {
+    close(fd);
+    return (-5);
+  }
+#elif defined(__FreeBSD__)
+
+  if (!up)
+    ;
+
+  char devname[strlen(ifname) + 6];
+  snprintf(devname, sizeof devname, "/dev/%s", ifname);
+  fd = open(devname, O_RDWR | O_NONBLOCK);
+  if (fd == -1)
+    return (-3);
+#elif defined(__OpenBSD__)
+  if (!up)
+    return (-2);
+
+  char devname[strlen(ifname) + 6];
+  snprintf(devname, sizeof devname, "/dev/%s", ifname);
+  fd = open(devname, O_RDWR | O_NONBLOCK);
+
+  if (fd == -1)
+    return (-3);
+#endif
+
+  return (fd);
+}
+
+value miou_solo5_net_read(intnat fd, value vbstr, intnat off, intnat len, value vread_size) {
+  CAMLparam1(vread_size);
+  ssize_t result = 0;
+  size_t size = len;
+  uint8_t *buf = (uint8_t *)Caml_ba_data_val(vbstr) + off;
+  result = read(fd, buf, size);
+  if ((result == 0) || (result == -1 && errno == EAGAIN)) {
+    ssize_t tmp = 0;
+    memcpy(Bytes_val(vread_size), (ssize_t *)&tmp, sizeof(ssize_t));
+    result = 2;
+  } else {
+    memcpy(Bytes_val(vread_size), (uint64_t *)&result, sizeof(uint64_t));
+    result = 0;
+  }
+  CAMLreturn(Val_long(result));
+}
+
+void miou_solo5_net_write(intnat fd, intnat off, intnat len, value vbstr) {
+  size_t size = len;
+  ssize_t result;
+  uint8_t *buf = (uint8_t *)Caml_ba_data_val(vbstr) + off;
+  result = write(fd, buf, size);
+  assert (result == len);
+}
+
+intnat miou_solo5_clock_monotonic(__unit()) {
+#if defined(__linux__)
+  struct timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return ((uint64_t) ts.tv_sec
+          * (uint64_t) 1000000000LL
+          + (uint64_t) ts.tv_nsec);
+#else
+  return 0;
+#endif
+}
+
+intnat miou_solo5_clock_wall(__unit()) { return 0; }
+
+#include <sys/select.h>
+#include <caml/alloc.h>
+#include <caml/misc.h>
+#include <caml/fail.h>
+#include <caml/callback.h>
+#include <math.h>
+#include <time.h>
+
+#define Nothing ((value) 0)
+
+static int fdlist_to_fdset(value fdlist, fd_set *fdset, int *maxfd)
+{
+  FD_ZERO(fdset);
+  for (value l = fdlist; l != Val_emptylist; l = Field(l, 1)) {
+    long fd = Long_val(Field(l, 0));
+    if (fd < 0 || fd >= FD_SETSIZE) return -1;
+    FD_SET((int) fd, fdset);
+    if (fd > *maxfd) *maxfd = fd;
+  }
   return 0;
 }
 
-intnat miou_solo5_clock_wall(__unit ()) {
-  return 0;
+static value fdset_to_fdlist(value fdlist, fd_set *fdset)
+{
+  CAMLparam0();
+  CAMLlocal2(l, res);
+
+  for (l = fdlist; l != Val_emptylist; l = Field(l, 1)) {
+    int fd = Int_val(Field(l, 0));
+    if (FD_ISSET(fd, fdset)) {
+      value newres = caml_alloc_small(2, Tag_cons);
+      Field(newres, 0) = Val_int(fd);
+      Field(newres, 1) = res;
+      res = newres;
+    }
+  }
+  CAMLreturn(res);
+}
+
+#define USEC_PER_SEC         1000000UL
+
+Caml_inline struct timeval caml_timeval_of_sec(double sec)
+{
+  double int_sec, frac_sec;
+  frac_sec = modf(sec, &int_sec);
+  return (struct timeval)
+    { .tv_sec  = (time_t) int_sec,
+      .tv_usec = (suseconds_t) (frac_sec * USEC_PER_SEC) };
+}
+
+void miou_solo5_error(int errcode, const char *cmdname, value cmdarg) {
+  CAMLparam0();
+  CAMLlocal2(name, arg);
+  value res;
+  const value *exn;
+  exn = caml_named_value("Miou_solo5.Solo5_error");
+  arg = cmdarg == Nothing ? caml_copy_string("") : cmdarg;
+  name = caml_copy_string(cmdname);
+  res = caml_alloc_small(4, 0);
+  Field(res, 0) = *exn;
+  Field(res, 1) = Val_int(errcode);
+  Field(res, 2) = name;
+  Field(res, 3) = arg;
+  caml_raise(res);
+  CAMLnoreturn;
+}
+
+CAMLprim value miou_solo5_select(value readfds, value timeout_sec)
+{
+  CAMLparam1(readfds);
+  fd_set read;
+  fd_set write;
+  fd_set except;
+  int maxfd;
+  double tm_sec;
+  struct timeval tv;
+  struct timeval * tvp;
+  int retcode;
+  value res;
+
+  FD_ZERO(&write);
+  FD_ZERO(&except);
+  maxfd = -1;
+  retcode = fdlist_to_fdset(readfds, &read, &maxfd);
+  if (retcode != 0) miou_solo5_error(0, "select", Nothing);
+  tm_sec = Double_val(timeout_sec);
+  if (tm_sec < 0.0) {
+    tvp = (struct timeval *) NULL;
+  } else {
+    tv = caml_timeval_of_sec(tm_sec);
+    tvp = &tv;
+  }
+  caml_enter_blocking_section();
+  retcode = select(maxfd + 1, &read, &write, &except, tvp);
+  caml_leave_blocking_section();
+  if (retcode == -1) miou_solo5_error(0, "select", Nothing);
+  readfds = fdset_to_fdlist(readfds, &read);
+  CAMLreturn(readfds);
 }
